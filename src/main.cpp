03c77bf8897323954e436cb140c1ecd2d932b1f5
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2025-08-19 19:56:08 +0200

    acceleration encoder

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,18 @@ long position = 0;
 long lastPosition = 0;
 int counter = 0;
 
+unsigned long lastMillis = 0;
+const int intervalMs = 50;
+bool lastButtonState = HIGH;
+
+// for momentum
+unsigned long lastStepTime = 0;
+
 void setup() {
   pinMode(SW_PIN, INPUT_PULLUP);
+  Serial.begin(9600); 
+  Serial.println("Servo tester");
+
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   display.setTextSize(1);
@@ -31,27 +41,40 @@ void setup() {
   display.display();
 }
 
-unsigned long lastMillis = 0;
-const int intervalMs = 50;
-bool lastButtonState = HIGH;
-
 void loop() {
   long newPosition = encoder.read() / STEPS_PER_CLICK;
   unsigned long currentMillis = millis();
   bool buttonState = digitalRead(SW_PIN);
 
   if (newPosition != lastPosition) {
-    if (newPosition < lastPosition) {
-      counter += 1;
-    } else {
-      counter -= 1;
-    }
+    int direction = (newPosition < lastPosition) ? 1 : -1;
+
+    unsigned long deltaT = currentMillis - lastStepTime;
+    lastStepTime = currentMillis;
+
+
+    int step = 1;
+    if (deltaT < 10)
+      step = 10; // super fast spin
+    else if (deltaT < 30)
+      step = 5; // fast
+    else if (deltaT < 100)
+      step = 2; // medium
+    else
+      step = 1; // slow
+
+    counter += direction * step;
     position = newPosition;
     lastPosition = newPosition;
+
+    Serial.print("Counter: ");
+    Serial.println(counter);
   }
 
   if (lastButtonState == HIGH && buttonState == LOW) {
-      counter = 0;
+    counter = 0;
+
+    Serial.println("Reset counter");
   }
 
   if (currentMillis - lastMillis >= intervalMs) {
